1209389ee2c596324762dee205130884bd4d9eeb
Jose Soto <jsoto7087@gmail.com>
2021-02-17 23:35:50 -0500

    commit tree 0b30e4c8784051e54c8a0e848a9a44ceafc2fec5

---
+++ lib/person.rb
@@ -0,0 +1,35 @@
+class Person
+
+
+  def initialize(name)
+    @name = name 
+  end 
+  
+end
+
+beyonce = Person.new("Beyonce")
+
+
+# class Person
+#   attr_accessor :name 
+#   def initialize(name)
+#     @name = name
+#   end 
+
+
+
+# end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
